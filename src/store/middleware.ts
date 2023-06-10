24d1bbc4265c57c0b2b181daf24b4025f67b4f26
PranitK <Pranit.Khadilkar@talentica.com>
2023-06-10 18:16:22 +0530

    added automatic logout when there is 401 api error

--- src/store/middleware.ts
+++ src/store/middleware.ts
@@ -1,7 +1,16 @@
-import { Middleware } from "@reduxjs/toolkit"
+import { Middleware, PayloadAction } from "@reduxjs/toolkit"
+import { AxiosResponse } from "axios"
+import { logout } from "../pages/login/login-slice"
 
-export const logger: Middleware = (api) => (next) => async (action) => {
-  // console.log("dispatching", action)
-  // console.log("next state", api.getState())
-  return next(action)
-}
+export const logger: Middleware =
+  (api) => (next) => async (action: PayloadAction<AxiosResponse>) => {
+    // console.log("dispatching", action)
+    // console.log("next state", api.getState())
+    if (
+      action.type.includes("rejected") &&
+      action.payload.data.statusCode === 401
+    ) {
+      api.dispatch(logout())
+    }
+    return next(action)
+  }
